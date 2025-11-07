c4ebe14c5a7fbc2bc59ab4021d3c2e8a4f7587ec
Matúš Laco <laco.matus@gmail.com>
2025-11-07 14:52:26 +0100

    route inf20ID

---
+++ src/app/about/page.tsx
@@ -0,0 +1,34 @@
+import FactCard from "../components/card";
+
+export default async function About() {
+  async function getFact(uniqueId = 1) {
+    const res = await fetch(
+      `https://uselessfacts.jsph.pl/api/v2/facts/random?t=${Date.now()}-${uniqueId*10}`,
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  }
+
+  const facts = await Promise.all([getFact(), getFact(), getFact()]);
+  const name = "John Doe";
+  return (
+    <>
+      <h1 className="text-5xl font-bold">About </h1>
+      <p className="mt-4 max-w-sm text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        reprehenderit eveniet odit laudantium ad illum consectetur fugiat harum
+        laboriosam quibusdam. Possimus sequi dolore corporis velit, hic id
+        mollitia deleniti harum!
+      </p>
+      {[0, 1, 2].map((item) => (
+        <FactCard
+          key={item}
+          factText={facts[item]?.text || ""}
+          author={facts[item]?.source || ""}
+        />
+      ))}
+    </>
+  );
+}
---
+++ src/app/components/card.tsx
@@ -0,0 +1,8 @@
+export default function FactCard({ factText, author }) {
+  return (
+    <div className="bg-gray-800 border border-gray-700 rounded-lg p-6 m-4 max-w-2xl w-full shadow-lg">
+      <p className="text-lg italic text-cyan-300">"{factText}"</p>
+      <p className="text-right mt-4 text-gray-400">- {author}</p>
+    </div>
+  );
+}
