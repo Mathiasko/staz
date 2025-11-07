128f6002c4fcea8c2930308d81e842cf8c0821f6
Matúš Laco <laco.matus@gmail.com>
2025-11-07 14:54:30 +0100

    route inf20ID

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,4 +1,4 @@
-export default function FactCard({ factText, author }) {
+export default function FactCard({ factText, author }: { factText: string, author: string }) {
   return (
     <div className="bg-gray-800 border border-gray-700 rounded-lg p-6 m-4 max-w-2xl w-full shadow-lg">
       <p className="text-lg italic text-cyan-300">"{factText}"</p>
